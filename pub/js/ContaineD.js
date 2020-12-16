8d6bc3b628ebf4711a57f4ffa9ac28556a12efdb
Wafiqah Raisa <wafiqah.raisa@mail.utoronto.ca>
2020-12-16 11:27:36 -0500

    got some css animations

--- pub/js/ContaineD.js
+++ pub/js/ContaineD.js
@@ -1,8 +1,156 @@
-function ContaineD() {
+function ContaineD(gridElement, options) {
+  //gridElement.style.display = "grid"
+  //gridElement.style.justifyItems = "start"
   this.domTree = document.getElementById("element-in-hierarchy")
+  if((options.hasOwnProperty('numContainers')) & (options.hasOwnProperty('containerSize'))) { // initializes the actual containers/ squares in the grid
+    this.createGrid(gridElement, options.numContainers, options.containerSize)
+  }
+
+  if(options.hasOwnProperty('gridColumnGap')){
+    this.applyColumnGap(gridElement, options.gridColumnGap) 
+  }
+  if(options.hasOwnProperty('gridRowGap')){
+    this.applyRowGap(gridElement, options.gridRowGap) 
+  }
+  if(options.hasOwnProperty('isCentered')){
+    if (options.isCentered) {
+      this.applyCentered(gridElement) 
+    }
+  }
+
+  if ((options.hasOwnProperty('columnSpacing'))) { // columnSpacing gets overriden by repeatColumns, and also by minColumnSize
+    if (options.hasOwnProperty('repeatColumns')) { // check if there is repeatColumns, if there is, need to apply that instead of columnSpacing and instead of minColumnSize (overrides minColumnSize and columnSpacing)
+      this.applyRepeatedColumns(gridElement, options.repeatColumns)
+    }
+    else if (options.hasOwnProperty('minColumnSize')) { // check if there is minColumnSize, if there is, need to apply that alongside columnSpacing.
+      this.applyMinColumnSize(gridElement, options.minColumnSize)
+      this.applyColumnSpace(gridElement, options.columnSpacing)
+    }
+    else if ((!(options.hasOwnProperty('repeatColumns'))) & (!(options.hasOwnProperty('minColumnSize')))) { // if there was no repeatColumns, or hasMinColumnSize, then need to apply columnSpace
+      this.applyColumnSpace(gridElement, options.columnSpacing)
+    }
+  }
+
+  // forget about row spacing rn.
+  if(options.hasOwnProperty('rowSpacing')){
+    if(options.hasOwnProperty('hasMinRowSize')){
+      
+    } 
+  }
+
+
 }
 
 ContaineD.prototype = { // prototype just declares functions 
+
+  createGrid: function(gridElement, numContainers, containerSize) {
+    for (var i = 1; i <= numContainers; i++) {
+      const gridContainer = document.createElement("div") // create a div container
+      gridContainer.style.wordWrap = "break-word"
+      gridContainer.id = "grid-item grid-item-" + i 
+      gridContainer.textContent = "Grid-Item " + i
+      gridContainer.style.border = "solid black 1px"
+      gridContainer.style.overflow = "auto"
+      gridElement.append(gridContainer)
+    }
+  },
+
+  applyColumnSpace: function(gridElement, columnSpacing) {
+    var s = ""
+    for (var i = 0; i < columnSpacing.length; i++) { 
+      if (i == (columnSpacing.length - 1)) {
+        s += columnSpacing[i] + "px" 
+      }
+      else {
+        s += columnSpacing[i] + "px "
+      }
+    }
+    gridElement.style.gridTemplateColumns = s
+  },
+
+  applyMinColumnSize: function(gridElement, minColumnSize) {
+    gridElement.style.gridAutoColumns = `minmax(${minColumnSize}px, auto)`
+  },
+
+  applyRepeatedColumns: function(gridElement, repeatColumns) {
+    gridElement.style.gridTemplateColumns = `repeat(${repeatColumns[0]}, ${repeatColumns[1]}px)`
+  },
+
+  applyColumnGap: function(gridElement, columnGap) {
+    gridElement.style.gridColumnGap = columnGap + "px"
+  },
+
+  applyRowGap: function(gridElement, rowGap) {
+    gridElement.style.gridRowGap = rowGap + "px"
+  },
+
+  applyCentered: function(gridElement) {
+    gridElement.style.justifyContent = "center"
+  },
+
+  setGridCellColumnPositioning: function(gridElement, start, end) {
+    gridElement.style.gridColumnStart = start
+    gridElement.style.gridColumnEnd = end + 1 // must do +1 because it isn't obv to user otherwise
+  },
+
+  setGridCellRowPositioning: function(gridElement, start, end) {
+    gridElement.style.gridRowStart = start
+    gridElement.style.gridRowEnd = end + 1 // must do +1 because it isn't obv to user otherwise
+  },
+
+  setGridCellImage: function(gridElement, imagePath) {
+    var img = document.createElement("img"); 
+    img.src = imagePath;
+    img.style.maxWidth = "100%"
+    img.style.maxHeight = "100%"
+    img.style.display = "block"
+    gridElement.style.objectFit = "end"
+    gridElement.appendChild(img);
+  },
+
+  setGridElementContent: function(gridElement, content) {
+    gridElement.textContent = content
+  },
+
+  backgroundColourTransitionEffect: function(gridCell, startingColour, endingColour, speed, x) {
+    //gridCell.style.backgroundColor = `${startingColour}`
+    gridCell.style.animation = `${x} ${speed}s infinite`;
+    var cssAnimation = document.createElement('style')
+    cssAnimation.type = 'text/css'
+    var rules = document.createTextNode(
+      `@-webkit-keyframes ${x} {` +
+      `0% { background-color: ${startingColour}; }` +
+      `50% { background-color: ${endingColour}; }` +
+      `100% { background-color: ${startingColour}; }` +
+      "}"
+      );
+    cssAnimation.appendChild(rules);
+    gridCell.appendChild(cssAnimation);
+  },
+
+  openCoverEffect: function(gridCell, gridElement) {
+    gridCell.classList.add("triggerOpenCoverEffect");
+    
+    var triggerDiv = document.createElement("div")
+    triggerDiv.classList.add("triggerOpenCoverEffect");
+
+
+    var dummyDiv = document.createElement("div")
+    dummyDiv.style.backgroundColor = "pink"
+    dummyDiv.style.height = "100px"
+    dummyDiv.style.width = "100px"
+    dummyDiv.classList.add("openCoverEffect");
+    gridElement.append(dummyDiv)
+
+
+    gridElement.style.height = "500px"
+    gridElement.style.width = "500px"
+
+    //gridElement.appendChild(dummyDiv)
+    console.log("height: ", gridElement.style.height)
+    console.log("width: ", gridElement.style.width)
+  },
+
 
   selectMode: function () { // select whether to be in debug mode or creative mode
 
@@ -22,9 +170,8 @@ ContaineD.prototype = { // prototype just declares functions
     creativeButton.style.margin = "7px"
     modeDiv.append(creativeButton)
 
-
   },
- 
+
 
   createHierarchy: function (element) { // from line 7 to line 15, this is not my code, I got it from https://jsfiddle.net/nkys3jrL/10/, the function walkElement() which are from line 14 to line 28
     var span = document.createElement("span")
@@ -39,15 +186,24 @@ ContaineD.prototype = { // prototype just declares functions
     }
   },
 
-  displayHTMLText: function () {
+  displayHTMLText: function (str) {
+    var HTMLDivTag = document.createElement("div")
+    HTMLDivTag.id = "HTMLDivTag"
+    this.domTree.appendChild(HTMLDivTag)
+
     var p = document.createElement("p")
-    p.textContent = "HTML: "
-    this.domTree.appendChild(p)
+    p.id = "HTMLTextDisplay"
+    p.textContent = str
+    HTMLDivTag.appendChild(p)
+
   },
 
 
-  displayElement: function(e) {
+  displayElement: function(e, viewCSSButton) {
     const target = e.target
+    var HTMLDivTag = document.getElementById("HTMLDivTag")
+
+    console.log("target: ", target)
     if (target.attachedDomTreeElement) {
 
       var toRemove = document.getElementById("elementCurrentlyViewing") // id is just one thing u need class.
@@ -73,7 +229,8 @@ ContaineD.prototype = { // prototype just declares functions
       var para = document.createElement("para")
       para.textContent = "Currently viewing: " + "<" + target.tagName.toLowerCase() + attributeString + ">\n"
       para.setAttribute('id', 'elementCurrentlyViewing');
-      this.domTree.appendChild(para)
+      HTMLDivTag.appendChild(para)
+      console.log("CSS of currently viewing: ", target.style)
 
       // parent element of the element we are currently viewing
       var attributeString = ""
@@ -83,7 +240,7 @@ ContaineD.prototype = { // prototype just declares functions
       var para = document.createElement("para")
       para.textContent = "Direct parent: " + "<" + target.parentElement.tagName.toLowerCase() + attributeString + ">\n"
       para.setAttribute('id', 'elementDirectParent');
-      this.domTree.appendChild(para)
+      HTMLDivTag.appendChild(para)
 
 
 
@@ -120,23 +277,90 @@ ContaineD.prototype = { // prototype just declares functions
         div.appendChild(para)
       }
 
-      this.domTree.appendChild(div)
+      HTMLDivTag.appendChild(div)
     }
+
+    console.log("view CSS : ", viewCSSButton)
+
+    viewCSSButton.id = "viewCSSButton"
+    viewCSSButton.style.marginTop = "10px"
+    
+    viewCSSButton.textContent = "Change CSS"
+    HTMLDivTag.append(viewCSSButton)
+    return target
   },
 
-
   // creative mode.
-  gridMode: function(e) { // allows for CSS grid style 
-    var ptag = document.createElement("p")
-    ptag.textContent = "Creative Mode"
+  gridMaker: function(numContainers, columnSpacing, rowSpacing, repeatColumns = [], hasMinColumnSize = 0, hasMinRowSize = 0, gridColumnGap=0, gridRowGap=0) { // takes in: number of containers (num), spacing btw columns (array), rowSpacing (array), whether row sizes should have a min value (initially 0)
+    // create a grid with numContainers amount of containers. You can also allocate the grid into columns, and the sizes of the columns can be listed
+    // in an array and passed in as columnSpacing. The number of columns is the length of the array columnSpacing. Similarly for rowSpacing. hasMinRowSize is
+    // default 0, this argument lets you set a minimum row size and allow elements in the grid to grow to any size based on its contents. If rowSpacing is passed in,
+    // hasMinRowSize will override it. gridcolumnGap is the amount of spacing between columns (int), default is set to 0. Similarly for gridRowGap.
 
-    const target = e.target 
+    console.log("in grid mode")
+    // tell the user to drop this HTML in their code, then tell them to call this function.
+    const containeDDiv = document.getElementById("containeD")
+    containeDDiv.style.display = "grid"
+
+    if (hasMinColumnSize > 0) { // hasMinColumnSize overrides columnSpacing
+      console.log("repeatColumns: ", repeatColumns)
+      if (repeatColumns.length > 0) { 
+        containeDDiv.style.gridTemplateColumns = `repeat(${repeatColumns[0]}, ${repeatColumns[1]}px)`
+      }
+      console.log("hasMinColumnSize: ", hasMinColumnSize)
+      containeDDiv.style.gridAutoColumns = `minmax(${hasMinColumnSize}, auto)`
+    }
+    else if (repeatColumns.length == 0) { // repeatColumns overrides columnSpacing 
+      var s = ""
+      for (var i = 0; i < columnSpacing.length; i++) { 
+        if (i == (columnSpacing.length - 1)) {
+          s += columnSpacing[i] + "px" 
+        }
+        else {
+          s += columnSpacing[i] + "px "
+        }
+      }
+      containeDDiv.style.gridTemplateColumns = s
+    }
+    else if (repeatColumns.length > 0) {
+      containeDDiv.style.gridTemplateColumns = `repeat(${repeatColumns[0]}, ${repeatColumns[1]}px)`
+    }
+    
+
+    if (hasMinRowSize > 0) {
+      containeDDiv.style.gridAutoRows = `minmax(${hasMinRowSize}, auto)`
+    }
+    else {
+      var s = ""
+      for (var i = 0; i < rowSpacing.length; i++) { 
+        if (i == rowSpacing.length - 1) {
+          s += rowSpacing[i] + "px" 
+        }
+        else {
+          s += rowSpacing[i] + "px "
+        }
+      }
+      console.log("R: ", s)
+      containeDDiv.style.gridTemplateRows = s
+    }
+
+    containeDDiv.style.gridColumnGap = gridColumnGap + "px"
+    containeDDiv.style.gridRowGap = gridRowGap + "px"
+
+
+    // for (var i = 1; i <= numContainers; i++) {
+    //   const gridContainer = document.createElement("div") // create a div container
+    //   gridContainer.style.wordWrap = "break-word"
+    //   gridContainer.id = "grid-item grid-item-" + i 
+    //   gridContainer.textContent = "Grid-Item " + i + "hehehehe Lorem50 xdaouighv iasdjhjk asihdba ksjdsdjhvgjhvagj "
+    //   gridContainer.style.border = "solid black 1px"
+    //   containeDDiv.append(gridContainer)
+    //   console.log("i: ", i)
+    // }
+
+
 
   }
-
-
-
-
 
 
 }
--- pub/js/example.js
+++ pub/js/example.js
@@ -1,18 +1,103 @@
-var containeD = new ContaineD()
+var gridElement = document.getElementById("containeD")
+//gridElement.style.margin = "90px"
+
+var containeD = new ContaineD(gridElement, {
+	numContainers: 5,
+	containerSize: [500, 500],
+	columnSpacing: [100, 200],
+	rowSpacing: [100, 100],
+	repeatColumns: [3, 100],
+	minColumnSize: 100,
+	hasMinRowSize:  0,
+	gridColumnGap: 20,
+	gridRowGap: 20,
+	isCentered: true, // default false
+})
+
+var gridElement1 = document.getElementById("grid-item grid-item-1")
+//containeD.openCoverEffect(gridElement1, gridElement)
+
+
+var gridElement2 = document.getElementById("grid-item grid-item-2")
+const content = "hehexddd"
+//containeD.setGridElementContent(gridElement2, content)
+imgPath = "a.jpg"
+containeD.setGridCellImage(gridElement2, imgPath)
+containeD.backgroundColourTransitionEffect(gridElement2, "pink", "lightblue", 1, "kk")
+
+
+var gridElement3 = document.getElementById("grid-item grid-item-3")
+const startingRow = 1 // user wants the cell to begin at Row 1, 
+const endingRow = 2 // the Row user wants this cell to end at. they want it to end at Row 3 here, if this value is allowed to exceed number of Rows.
+containeD.setGridCellRowPositioning(gridElement3, startingRow, endingRow)
+
+const startingColumn = 2 // user wants the cell to begin at Row 1, 
+const endingColumn = 3 // the Row user wants this cell to end at. they want it to end at Row 3 here, if this value is allowed to exceed number of Rows.
+containeD.setGridCellColumnPositioning(gridElement3, startingColumn, endingColumn)
+
+imgPath = "b.jpg"
+containeD.setGridCellImage(gridElement3, imgPath)
+
+containeD.backgroundColourTransitionEffect(gridElement3, "pink", "blue", 3, "pp")
+
+
+var gridElement5 = document.getElementById("grid-item grid-item-5")
+const startColumn = 2
+const endColumn = 3
+containeD.setGridCellColumnPositioning(gridElement5, startColumn, endColumn)
+
+
+containeD.selectMode()
 
 var userWebsite = document.getElementById("userWebsite")
 var debugButton = document.getElementById("debugButton")
 var creativeButton = document.getElementById("creativeButton")
+var viewCSSButton = document.createElement("BUTTON")
 
-containeD.selectMode()
-containeD.displayHTMLText()
-containeD.createHierarchy(userWebsite)
-
+// containeD.displayHTMLText()
+// containeD.createHierarchy(userWebsite)
+var target = null
 userWebsite.addEventListener("click", function a(e) {
-	containeD.displayElement(e) // we call this function only when something gets clicked on, because when someone clicks on something, you'd wanna display that element
+	target = containeD.displayElement(e, viewCSSButton) // we call this function only when something gets clicked on, because when someone clicks on something, you'd wanna display that element
+
 })
+console.log("target: ", target)
+
 
 debugButton.addEventListener("click", function a(e) { // if someone clicks on this button, they want to be in debug mode, so that includes being able to click on HTML elements on see them.
+	document.getElementById("debugButton").disabled = true;
+	document.getElementById("creativeButton").disabled = false;
+
+	var toRemove = document.getElementById("HTMLDivTag") // this is here so that when you click on the button, everything that was in the container will disappear.
+	if (toRemove) {
+		toRemove.remove();
+	}
+
+	containeD.displayHTMLText("HTML: ")
+	containeD.createHierarchy(userWebsite)
 
 })
+
+viewCSSButton.addEventListener("click", function a(e) { 
+	console.log("CSS view button clicked")
+	containeD.viewCSS(target)
+})
+
+creativeButton.addEventListener("click", function a(e) { // if someone clicks on this button, they want to be in debug mode, so that includes being able to click on HTML elements on see them.
+	document.getElementById("creativeButton").disabled = true;
+	document.getElementById("debugButton").disabled = false;
+
+	var toRemove = document.getElementById("HTMLDivTag") 
+	if (toRemove) {
+		toRemove.remove();
+	}
+	
+	containeD.displayHTMLText("Creative Mode")
+	//containeD.createHierarchy(userWebsite)
+	//containeD.gridMaker()
+})
+
+
+
+
 
